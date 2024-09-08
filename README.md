5836834a6130166b9d2eaebc69fa7a41b1d050c5
roshnirajan1106 <roshnirajan8888@gmail.com>
2024-09-08 23:26:14 +0530

    initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # spring-rest-crud-api
+request - request line, header , message body
+1. request = get/post/what method
+2. header = general meta data = type of content -xml / json
+3. message body = json data 
+
+same for respose
+ 
---
+++ restCrud/src/test/java/com/restCrudApi/demo/RestCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restCrudApi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
