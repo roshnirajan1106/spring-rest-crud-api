e1af895589e493a4b73860207fc22e362d4390d6
roshnirajan1106 <roshnirajan8888@gmail.com>
2024-09-09 10:26:34 +0530

    added path variables

--- README.md
+++ README.md
@@ -6,8 +6,17 @@ request - request line, header , message body
 
 same for respose
  
-java json data binding 
+<h2>java json data binding </h2>
 1. process of converting json data to a java pojo
 2. jackson, binds both db object and java object.
 3. json data passed to rest contoller is converted into pojo and vice versa
 4. jackson dependency is included in spring boot starter web
+
+<h2>Path Variables</h2>
+localhost:8081/api/students/{PathVaraible}
+for example
+    localhost:8081/api/students/0
+    localhost:8081/api/students/1
+
+<h2>Post Construct</h2>
+this annotated method is called only once after the bean is created
--- restCrud/src/main/java/com/restCrudApi/demo/rest/StudentRestController.java
+++ restCrud/src/main/java/com/restCrudApi/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.restCrudApi.demo.rest;
 
 import com.restCrudApi.demo.enitity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,25 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Roshini","Rajan"));
         theStudents.add(new Student("Viji","Rajan"));
         theStudents.add(new Student("Parthi","Rajan"));
+    }
+
+    @GetMapping("/students{}")
+    public List<Student> getStudents(){
+
         return theStudents;
     }
+    //retrieve a single student
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
+    }
+
 }
